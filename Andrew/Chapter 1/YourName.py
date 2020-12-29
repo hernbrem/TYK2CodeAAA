efb8252326ad03ec7985515ff67b4ea722b346e7
hernbrem <76752866+hernbrem@users.noreply.github.com>
2020-12-29 16:06:57 -0600

    Andrew and Bretts chapter 1
    Committed Andrew and Brett's chapter 1 files

---
+++ Andrew/Chapter 1/YourName.py
@@ -0,0 +1,14 @@
+#YourName.py
+name = input("What is your name?\n")
+print("Hi, ", name) 
+FavoriteColor  = input("What is your favorite color?\n")
+HowDoing = input("How are you?\n")
+FavoriteAnimal = input("What is your favorite animal?\n")
+FavoritePottyWord = input("What is your favorite potty verb that ends in -ed?\n")
+FavoriteShape = input("What is your favorite shape?\n")
+FavoriteActionVerb = input("What is your favorite action verb?\n")
+FavoriteNoun = input("What is your favorete noun?\n")
+
+
+
+#print"(once upon a time there whas a                                               
---
+++ Uncle_Brett/Chapter 1/DadMadlib.py
@@ -0,0 +1,8 @@
+# DadMadlib.py
+potty_verb_ed = input("What is your favorite potty verb that ends in -ed?\n")
+adverb = input("What is an awesome adverb?\n")
+big_bathroom_nown = input("What is a big nown that can fit in a bathroom?\n")
+adjective = input("What is a funny adjective?\n")
+
+print("Yesterday there was a", adjective, big_bathroom_nown, "who made everyone laugh when they", adverb, potty_verb_ed, " and forgot to close the bathroom door.")
+
