6bd4d1737925d7e6109db9b7e4515ac91e51b1da
hernbrem <76752866+hernbrem@users.noreply.github.com>
2021-05-23 18:27:42 -0500

    Removed the background of audrey's ship
    Edited the space ship in paint 3d

---
+++ Uncle_Brett/Pygame/TechWithTimPygameForBeginners/space_battle_constants.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 16:05:14 2021
+
+@author: brett
+"""
+
+WIDTH, HEIGHT = 900, 500
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+FPS = 60
+VEL = 5
+MAX_BULLETS = 5
+BULLET_VEL = 10
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
